38afc2491b527975f5b6e5f66692f0c9142c248b
velimah <veli-matti.heino@metropolia.fi>
2023-01-17 14:47:02 +0200

    week2-task2

--- src/index.js
+++ src/index.js
@@ -9,21 +9,30 @@ const array =
     {name: 'Pureed root vegetable soup with smoked cheese', price: 8.00}
   ];
 
+
 let validator = (string) => {
   let regexp = /^[A-ZÄÖÅ][A-ZÄÖÅa-zäöå0-9-/,()\s]{3,63}$/;
   return regexp.test(string);
 };
 
-const sortedByPrice = array.sort((a, b) => a.price - b.price);
+const sortedByPrice = array.sort((a, b) => {
+  return a.price - b.price;
+});
 console.log('Sorted by price', sortedByPrice);
 
-const filtered = array.filter(item => item.price <= 5);
+const filtered = array.filter(item => {
+  return item.price <= 5;
+});
 console.log('Costing max 5e', filtered);
 
-const multiplied = array.map(number => parseFloat((number.price * 1.15).toFixed(2)));
+const multiplied = array.map(item => {
+  item.price = parseFloat((item.price * 1.15).toFixed(2));
+  return item;
+});
 console.log('Prices increased by 15%', multiplied);
 
-const sum = array.reduce((a, b) => ({price: a.price + b.price}));
-console.log('Total cost of menu', sum);
-
-
+const sum = array.reduce((acc, curr) => {
+  acc.price += curr.price;
+  return acc;
+}, {price: 0});
+console.log('Total cost of menu', sum.price);
