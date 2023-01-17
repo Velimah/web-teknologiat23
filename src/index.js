7d747f25adb4f7fa73034b05fff4f020a28eda95
velimah <veli-matti.heino@metropolia.fi>
2023-01-17 14:31:31 +0200

    week2-task2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('Hello ace of base!');
+'use strict';
+
+const array =
+  [
+    {name: 'Lingonberry jam', price: 4.00},
+    {name: 'Mushroom and bean casserole', price: 5.50},
+    {name: 'Chili-flavoured wheat', price: 3.00},
+    {name: 'Vegetarian soup', price: 4.80},
+    {name: 'Pureed root vegetable soup with smoked cheese', price: 8.00}
+  ];
+
+let validator = (string) => {
+  let regexp = /^[A-ZÄÖÅ][A-ZÄÖÅa-zäöå0-9-/,()\s]{3,63}$/;
+  return regexp.test(string);
+};
+
+const sortedByPrice = array.sort((a, b) => a.price - b.price);
+console.log(sortedByPrice);
+
+const filtered = array.filter(item => item.price <= 5);
+console.log(filtered);
+
+const multiplied = array.map(number => parseFloat((number.price * 1.15).toFixed(2)));
+console.log(multiplied);
+
+const sum = array.reduce((a, b) => ({price: a.price + b.price}));
+console.log(sum);
+
+
