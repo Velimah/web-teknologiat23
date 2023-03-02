c57a07cb30b1d9fe9de9a7af96d9d0dbc0707e43
velimah <veli-matti.heino@metropolia.fi>
2023-03-02 20:30:25 +0200

    räjäytetty

--- src/modules/fetch-lunchmenu.js
+++ src/modules/fetch-lunchmenu.js
@@ -9,7 +9,16 @@ let fazerDataFiArabia;
 let fazerDataEnArabia;
 
 const getLunchMenus = async () => {
+  await getSodexoMyllypuroMenu();
+  await getSodexoMyyrmakiMenu();
+  await getFazerKaramalmiMenuFi();
+  await getFazerKaramalmiMenuEn();
+  await getFazerArabiaMenuFi();
+  await getFazerArabiaMenuEn();
+};
 
+// fetches sodexo myllypuro menu
+const getSodexoMyllypuroMenu = async () => {
   // fetches sodexo myllypuro menu
   try {
     sodexoDataMyllypuro = await doFetch(
@@ -19,8 +28,12 @@ const getLunchMenus = async () => {
     console.log('sodexo menu myllypuro', sodexoDataMyllypuro);
   } catch (error) {
     console.log('menu ei saatavilla');
+    setTimeout(() => getSodexoMyllypuroMenu(), 5000);
   }
-  // fetches sodexo myyrmaki menu
+};
+
+// fetches sodexo myyrmaki menu
+const getSodexoMyyrmakiMenu = async () => {
   try {
     sodexoDataMyyrmaki = await doFetch(
       `https://www.sodexo.fi/ruokalistat/output/weekly_json/${myyrmakiSettings.id}`,
@@ -29,9 +42,12 @@ const getLunchMenus = async () => {
     console.log('sodexo menu myyrmaki', sodexoDataMyyrmaki);
   } catch (error) {
     console.log('menu ei saatavilla');
+    setTimeout(() => getSodexoMyyrmakiMenu(), 5000);
   }
+};
 
-  // fetches finnish foodco arabia menu
+// fetches finnish foodco arabia menu
+const getFazerArabiaMenuFi = async () => {
   try {
     fazerDataFiArabia = await doFetch(
       `https://www.compass-group.fi/menuapi/feed/json?costNumber=${arabiaSettings.id}&language=fi`,
@@ -40,9 +56,12 @@ const getLunchMenus = async () => {
     console.log('foodco menu arabia finnish', fazerDataFiArabia);
   } catch (error) {
     console.log('menu ei saatavilla');
+    setTimeout(() => getFazerArabiaMenuFi(), 5000);
   }
+};
 
-  // fetches english foodco arabia menu
+// fetches english foodco arabia menu
+const getFazerArabiaMenuEn = async () => {
   try {
     fazerDataEnArabia = await doFetch(
       `https://www.compass-group.fi/menuapi/feed/json?costNumber=${arabiaSettings.id}&language=en`,
@@ -51,9 +70,12 @@ const getLunchMenus = async () => {
     console.log('foodco menu arabia english', fazerDataEnArabia);
   } catch (error) {
     console.log('menu ei saatavilla');
+    setTimeout(() => getFazerArabiaMenuEn(), 5000);
   }
+};
 
-  // fetches finnish foodco karamalmi menu
+// fetches finnish foodco karamalmi menu
+const getFazerKaramalmiMenuFi = async () => {
   try {
     fazerDataFiKaramalmi = await doFetch(
       `https://www.compass-group.fi/menuapi/feed/json?costNumber=${karamalmiSettings.id}&language=fi`,
@@ -62,9 +84,11 @@ const getLunchMenus = async () => {
     console.log('foodco menu karamalmi finnish', fazerDataFiKaramalmi);
   } catch (error) {
     console.log('menu ei saatavilla');
+    setTimeout(() => getFazerKaramalmiMenuFi(), 5000);
   }
-
-  // fetches english foodco karamalmi menu
+};
+// fetches english foodco karamalmi menu
+const getFazerKaramalmiMenuEn = async () => {
   try {
     fazerDataEnKaramalmi = await doFetch(
       `https://www.compass-group.fi/menuapi/feed/json?costNumber=${karamalmiSettings.id}&language=en`,
@@ -73,6 +97,7 @@ const getLunchMenus = async () => {
     console.log('foodco menu karamalmi english', fazerDataEnKaramalmi);
   } catch (error) {
     console.log('menu ei saatavilla');
+    setTimeout(() => getFazerKaramalmiMenuEn(), 5000);
   }
 };
 
